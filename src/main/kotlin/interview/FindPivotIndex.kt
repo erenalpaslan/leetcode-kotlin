645c0c87b24bacfe47f999b0e5726afc37c6fc82
Eren <eren.alparslan@appcent.mobi>
2023-03-04 16:07:56 +0300

    -Solves find pivot index challenge

--- src/main/kotlin/interview/FindPivotIndex.kt
+++ src/main/kotlin/interview/FindPivotIndex.kt
@@ -5,6 +5,33 @@ package interview
  */
 object FindPivotIndex {
     fun pivotIndex(nums: IntArray): Int {
+        var pivotIndex = -1
+        for (index in nums.indices) {
+            val sumOfLeft = sumLeftOfIndex(nums, index)
+            val sumOfRight = sumRightOfIndex(nums, index)
+            if (sumOfLeft == sumOfRight) {
+                pivotIndex = index
+                break
+            }
+        }
 
+        return pivotIndex
     }
+
+    private fun sumLeftOfIndex(nums: IntArray, index: Int): Int {
+        return if (index != 0) {
+            nums.slice(0 until index).sum()
+        }else {
+            0
+        }
+    }
+
+    private fun sumRightOfIndex(nums: IntArray, index: Int): Int {
+        return if (index == nums.lastIndex) {
+            0
+        } else {
+            nums.slice(index + 1..nums.lastIndex).sum()
+        }
+    }
+
 }
--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -5,6 +5,5 @@ import interview.ClimbingStairs.climbStairs
 import interview.MergeTwoSortedList.printNode
 
 fun main(args: Array<String>) {
-    val X = RunningSumOf1dArray.runningSum(intArrayOf(1,2,3,4))
-    println("=>${X.toMutableList()}")
+    println(FindPivotIndex.pivotIndex(intArrayOf(-1,-1,0,0,-1,-1)))
 }
