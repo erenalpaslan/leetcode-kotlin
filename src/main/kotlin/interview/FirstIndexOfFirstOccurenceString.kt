0809277369f3b893a76d422f4cb3d1842075c16a
Eren <eren.alparslan@appcent.mobi>
2023-03-19 12:18:56 +0300

    -Solves 28 first index of first occurence in string

---
+++ src/main/kotlin/interview/FirstIndexOfFirstOccurenceString.kt
@@ -0,0 +1,10 @@
+package interview
+
+/**
+ * Created by erenalpaslan on 19.03.2023
+ */
+object FirstIndexOfFirstOccurenceString {
+    fun strStr(haystack: String, needle: String): Int {
+        return haystack.indexOf(needle)
+    }
+}
